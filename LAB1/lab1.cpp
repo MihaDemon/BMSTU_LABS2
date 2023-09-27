2de7deaa3744fec8830e2c87639d5c9d0b1f377f
Mikhail Delyanchenko <mikhail.delyanchenko@gmail.com>
2023-09-27 18:42:22 +0300

    LAB1 DONE

--- LAB1/lab1.cpp
+++ LAB1/lab1.cpp
@@ -27,7 +27,7 @@ void task3()
 
 void task4_1()
 {
-    int a, x;
+    double a, x;
 
     cout<< "Input value for a: ";
     cin>> a;
@@ -39,7 +39,7 @@ void task4_1()
 
 void task4_2()
 {
-    int y, x;
+    double y, x;
 
     cout<< "Input value for x: ";
     cin>> x;
@@ -51,7 +51,7 @@ void task4_2()
 
 void task5()
 {
-    int weight, volume;
+    double weight, volume;
 
     cout<< "Input weight: ";
     cin>> weight;
@@ -62,14 +62,152 @@ void task5()
     cout<< "Density: " << weight / volume << "kg/m^3" << endl;
 }
 
+void task6()
+{
+    double a, b, x;
+
+    cout<< "Input a: ";
+    cin>> a;
+
+    cout<< "Input b: ";
+    cin>> b;
+
+    x = -b / a;
+
+    cout<< "Result: " << x << endl;
+}
+
+struct dot
+    {
+        double x, y;
+    };
+
+std::istream &operator>> (std::istream &in, dot &obj)
+    {
+        cout<< "Input x coord: ";
+        cin>> obj.x;
+
+        cout<< "Input y coord: ";
+        cin>> obj.y;
+
+        return in;
+    }
+
+void task7()
+{
+    dot first, second;
+
+    cout<< "Input first dot coords" << endl;
+    cin>> first;
+
+    cout<< "Input second dot coords" << endl;
+    cin>> second;
+
+    double length;
+
+    length = std::sqrt( std::pow( second.x - first.x, 2) + std::pow( second.y - first.y, 2) );
+
+    std::cout<< "Result: " << length << endl; 
+}
+
+void task8()
+{
+    double a, b, h;
+
+    cout<< "Input a: ";
+    cin>> a;
+
+    cout<< "Input b: ";
+    cin>> b;
+
+    cout<< "Input h: ";
+    cin>> h;
+
+    double x = (b - a) / 2;
+
+    double c = std::sqrt(h * h + x * x);
+
+    double P = a + b + 2 * c;
+
+    cout<< "Result: " << P << endl;
+}
+
+void task9()
+{
+    double a;
+
+    cout<< "Input a: ";
+    cin>> a;
+
+    double S = a * a * a;
+
+    double S2 = 6 * a * a;
+
+    cout<< "S: " << S << endl;
+    cout<< "S2: " << S2 << endl;
+}
+
+void task10()
+{
+    double a;
+
+    cout<< "Input a: ";
+    cin>> a;
+
+    double P = 4 * a;
+
+    cout<< "Result: " << P << endl;
+}
+
+void task11()
+{
+    double r;
+
+    cout<< "Input r: ";
+    cin>> r;
+
+    double d = 2 * r;
+
+    cout<< "Resutl: " << d << endl;
+}
+
+void task12()
+{
+    std::string name;
+
+    cout<< "Input your name: ";
+    cin>> name;
+
+    cout<< "Hello, " << name << "! My name is C++." << endl;
+}
+
 int main()
 {
+    cout<< "Task 1" << endl;
     task1();
+    cout<< "Task 2" << endl;
     task2();
+    cout<< "Task 3" << endl;
     task3();
+    cout<< "Task 4" << endl;
     task4_1();
     task4_2();
+    cout<< "Task 5" << endl;
     task5();
+    cout<< "Task 6" << endl;
+    task6();
+    cout<< "Task 7" << endl;
+    task7();
+    cout<< "Task 8" << endl;
+    task8();
+    cout<< "Task 9" << endl;
+    task9();
+    cout<< "Task 10" << endl;
+    task10();
+    cout<< "Task 11" << endl;
+    task11();
+    cout<< "Task 12" << endl;
+    task12();
 
     return 0;
 }
